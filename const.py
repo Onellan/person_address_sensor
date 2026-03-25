6bc7243331e7159041f59733de5e829eebc7a4dc
OG <25457961+Onellan@users.noreply.github.com>
2026-03-25 06:05:22 +0200

    Create const.py

---
+++ const.py
@@ -0,0 +1,13 @@
+DOMAIN = "person_address_sensor"
+
+CONF_PERSONS = "persons"
+CONF_PERSON = "person"
+CONF_INTERVAL = "interval"
+CONF_TEMPLATE = "format_template"
+CONF_DISTANCE_THRESHOLD = "distance_threshold"
+
+DEFAULT_INTERVAL = 300
+DEFAULT_DISTANCE_THRESHOLD = 50
+MINIMUM_INTERVAL = 300
+CACHE_FILE = "person_address_cache.json"
+CACHE_TIMEOUT = 86400
